1fd2968f42025724b1c9ea5ff7b49f8e423bb755
newhappy-sudo <4balless@gmail.com>
2026-02-15 17:26:47 +0100

    test4

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,35 @@
 // pages/_app.js
-import '@solana/wallet-adapter-react-ui/styles.css';
-import '../styles/minimal.css';
+import { WalletAdapterNetwork } from '@solana/wallet-adapter-base';
+import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react';
+import { WalletModalProvider } from '@solana/wallet-adapter-react-ui';
+import { PhantomWalletAdapter } from '@solana/wallet-adapter-wallets';
+import { clusterApiUrl } from '@solana/web3.js';
+import { useMemo } from 'react';
 
-export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+// Import Wallet Adapter CSS
+import '@solana/wallet-adapter-react-ui/styles.css';
+
+// Import Bootstrap (si pas dans _document.js)
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
+
+// Import le CSS pour forcer le modal
+import '../styles/modal-force.css';
+
+function MyApp({ Component, pageProps }) {
+  const network = WalletAdapterNetwork.Mainnet;
+  const endpoint = useMemo(() => clusterApiUrl(network), [network]);
+  const wallets = useMemo(() => [new PhantomWalletAdapter()], []);
+
+  return (
+    <ConnectionProvider endpoint={endpoint}>
+      <WalletProvider wallets={wallets} autoConnect>
+        <WalletModalProvider>
+          <Component {...pageProps} />
+        </WalletModalProvider>
+      </WalletProvider>
+    </ConnectionProvider>
+  );
 }
+
+export default MyApp;
